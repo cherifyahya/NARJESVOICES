22813fefe029ab342202b1d3fe650f9b4c18c57a
yahya <cherifyahya069@gmail.com>
2024-10-21 18:28:38 +0100

    adding navigation bar

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "hello world"
 
-if __name__ == "__main__":
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',)
+
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
