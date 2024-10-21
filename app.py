6bfdd98153debdcd54c93f2971bb2f32061e7bb8
yahya <cherifyahya069@gmail.com>
2024-10-21 22:49:43 +0100

    add decorators

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 
@@ -8,6 +9,16 @@ app = Flask(__name__)
 def home():
     return render_template('home.html',)
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='login', form=form)
 
 
 
